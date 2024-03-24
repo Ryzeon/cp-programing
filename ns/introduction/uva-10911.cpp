dd076df4b4f174bad7ae0cc68abe3f7dd8d27fca
Ryzeon <alex.ramon2004@gmail.com>
2024-03-24 01:14:32 -0500

    uva

---
+++ ns/introduction/uva-10911.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define OS ios_base::sync_with_stdio(false); cin.tie(nullptr);
+#define ar array
+#define str string
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define fori(n) for (int i = 0; i < (int)(n); ++i)
+#define rfori(n) for (int i = (int)(n) - 1; i >= 0; --i)
+#define a first
+#define b second
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<pair<int,int>> p;
+    while (n * 2) {
+        pair<int, int> a;
+        cin >> a;
+    }
+}
+
+int main() {
+    OS;
+    int n = 1;
+   // cin >> n;
+    
+    while (n--) {
+        solve();
+    }
+    return 0;
+}
